1008fdadb1844f8ee4495cfc5b5639e58bb4b3b1
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-29 01:17:21 +0200

    More Optional and introduced a specific type for the shared-state in TaskWithoutSharedState

--- anchor-gui-browser/src/main/java/org/anchoranalysis/gui/interactivebrowser/launch/InteractiveBrowserTask.java
+++ anchor-gui-browser/src/main/java/org/anchoranalysis/gui/interactivebrowser/launch/InteractiveBrowserTask.java
@@ -35,6 +35,7 @@ import org.anchoranalysis.core.error.InitException;
 import org.anchoranalysis.experiment.JobExecutionException;
 import org.anchoranalysis.experiment.bean.task.TaskWithoutSharedState;
 import org.anchoranalysis.experiment.task.InputTypesExpected;
+import org.anchoranalysis.experiment.task.NoSharedState;
 import org.anchoranalysis.experiment.task.InputBound;
 import org.anchoranalysis.gui.interactivebrowser.browser.InteractiveBrowser;
 import org.anchoranalysis.gui.interactivebrowser.input.InteractiveBrowserInput;
@@ -54,7 +55,7 @@ public class InteractiveBrowserTask extends TaskWithoutSharedState<InteractiveBr
 	}
 		
 	@Override
-	public void doJobOnInputObject(InputBound<InteractiveBrowserInput,Object> params)	throws JobExecutionException {
+	public void doJobOnInputObject(InputBound<InteractiveBrowserInput,NoSharedState> params)	throws JobExecutionException {
 		
 		try {
 			InteractiveBrowser browser = new InteractiveBrowser(
